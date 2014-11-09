c8de9216fe316b87f1fd6be9ecd7118afa70479b
coursera2 <coursera2@users.noreply.github.com>
2014-11-08 20:12:43 -0500

    initial release of cached matrix (inverse) code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## R functions to construct a cached matrix and its inverse
+## in order to avoid the computational complexity of computing an
+## inverse each time it is needed
+## Revision History: initial release November 2014 coursera2
+## 
+## makeCacheMatrix(matrix)
+## constructor for matrix object with get/set and cached inverse
+## returns specialMatrixObject list of function pointers
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set=set, get=get, setinv=setinv, getinv=getinv)    
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve(specialMatrixObject)
+## function to compute and cache inverse of a matrix (if invertible)
+## returns inverse of matrix in specialMatrixObject
 cacheSolve <- function(x, ...) {
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
         ## Return a matrix that is the inverse of 'x'
 }
+
